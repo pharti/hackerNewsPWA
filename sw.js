45b5f458f064f218b4ce17d31d88ae572b70bc3f
Amit Singh <asphartiyal@gmail.com>
2021-03-31 12:26:44 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,40 @@
+var CACHE_NAME = "hackerNewsPWA";
+var urlsToCache = ["/", "index.html"];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  var cacheAllowlist = ["hackerNewsPWA"];
+
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
